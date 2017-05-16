5601cd81eb6ef2bfa21cc0680ad36870d4716ba9
John Thompson <john@springframework.guru>
2017-05-16 16:16:14 -0400

    adding JPA example

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -1,10 +1,18 @@
 package guru.springframework.spring5webapp.repositories;
 
+<<<<<<< HEAD
 import guru.springframework.spring5webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by jt on 12/23/19.
+=======
+import guru.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 5/16/17.
+>>>>>>> af9c8e8... adding JPA example
  */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
